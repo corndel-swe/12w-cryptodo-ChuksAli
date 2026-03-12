f0443270ce01a87d691046d59bec9d41818eaae3
Chuks Alichukwu <alichukwucp@gmail.com>
2026-03-12 11:37:59 +0000

    Middleware attempt commented out

---
+++ middleware/protect.js
@@ -0,0 +1,21 @@
+import { db } from '../db/config.js'
+import bcrypt from 'bcrypt' 
+
+
+// export const protect = async (req, res, next) => {
+//     const base64Credentials = req.headers.authorization.split(' ')[1]
+//     const [username, password] = atob(base64Credentials).split(':')
+
+//     const user = await db('users').where({ username }).first()
+//     if (!user) throw new Error(`User ${username} not found.`)
+
+//     const result = await bcrypt.compare(password, user.password)
+
+//     if (!result) {
+//       console.log("Passwords don't match");
+//       res.set('WWW-Authenticate', 'Basic realm="cryptodo"')
+//       return res.status(401).send('Invalid credentials.')
+//     } 
+
+//     else {next()}
+// }
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { renderUserTodos } from '../controllers/todo.controller.js'
 import { createTodo } from '../controllers/todo.controller.js'
+// import { protect } from '../middleware/protect.js'
 
 const router = Router()
 
-router.get('/', renderUserTodos)
+router.get('/',  renderUserTodos)
 
 router.get('/new', (req, res) => {
   res.render('todos/new')
